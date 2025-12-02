83b7f9a251baf0f033c917c08a1d1335d07ec76f
krishnamohan-10 <psrikrishnamohan_cse2305a7@mgit.ac.in>
2025-12-02 19:11:57 +0530

    Refactor encryption and decryption functions

--- data_encryption_decryption.c
+++ data_encryption_decryption.c
@@ -1,36 +1,26 @@
 #include <stdio.h>
 #include <string.h>
+void encrypt(char text[], int key) {
+   for (int i = 0; text[i] != '\0'; i++) {
 
-char shift(char c, int key) {
-    if (c >= 'a' && c <= 'z')
-        return (c - 'a' + key + 26) % 26 + 'a';
-    if (c >= 'A' && c <= 'Z')
-        return (c - 'A' + key + 26) % 26 + 'A';
-    return c; 
+       text[i] = text[i] + key;  
+   }
 }
-
+void decrypt(char text[], int key) {
+   for (int i = 0; text[i] != '\0'; i++) {
+       text[i] = text[i] - key;
+   }
+}
 int main() {
-    char text[100];
-    int key, i;
-
-    printf("enter a message: ");
-    fgets(text, sizeof(text), stdin);
-    text[strcspn(text, "\n")] = '\0';
-
-    printf("enter a key(1-25): ");
-    scanf("%d", &key);
-
-    // Encrypt
-    for (i = 0; text[i] != '\0'; i++)
-        text[i] = shift(text[i], key);
-
-    printf("encrypted message: %s\n", text);
-
-    // Decrypt
-    for (i = 0; text[i] != '\0'; i++)
-        text[i] = shift(text[i], -key);
-
-    printf("decrypted message: %s\n", text);
-
-    return 0;
+   char text[100];
+   int key;
+   printf("Enter text: ");
+   fgets(text, sizeof(text), stdin);
+   printf("Enter key (1-25): ");
+   scanf("%d", &key);
+   encrypt(text, key);
+   printf("\nEncrypted Text: %s\n", text);
+   decrypt(text, key);
+   printf("Decrypted Text: %s\n", text);
+   return 0;
 }
